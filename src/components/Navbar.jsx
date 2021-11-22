bd09b6e341f8d4c3c140f794d2f871f96d13ba5f
ADE <adejumoahmad4life@gmail.com>
2021-11-22 15:33:54 +0100

    added media queries to the navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  logoLg: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  logoSm: {
+    display: "block",
+    [theme.breakpoints.up("lg")]: {
+      display: "none",
+    },
+  },
+}));
 
 const Navbar = () => {
   const classes = useStyles();
   return (
     <AppBar>
       <Toolbar>
-        <Typography variant="h6">Ade's Site</Typography>;
-        <Typography variant="h6">ADE</Typography>;
+        <Typography variant="h6" className={classes.logoLg}>
+          Ade's Site
+        </Typography>
+        ;
+        <Typography variant="h6" className={classes.logoSm}>
+          ADE
+        </Typography>
+        ;
+        <div className={classes.search}>
+          <Search />
+          <InputBase />
+        </div>
       </Toolbar>
     </AppBar>
   );
