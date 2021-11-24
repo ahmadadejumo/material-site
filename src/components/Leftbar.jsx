1cc62fa1430baced7baca50acb055a3cd4e79621
ADE <adejumoahmad4life@gmail.com>
2021-11-24 16:43:57 +0100

    working on left bar component

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,11 +1,11 @@
-import { makeStyles } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({}));
 
 const Leftbar = () => {
   const classes = useStyles();
-  return <div></div>;
+  return <Container></Container>;
 };
 
 export default Leftbar;
