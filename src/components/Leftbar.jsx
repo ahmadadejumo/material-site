1af7f9973c68a1426662d0663414edc4e73ac98e
ADE <adejumoahmad4life@gmail.com>
2021-11-24 16:49:35 +0100

    added icons to leftbar component

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,11 +1,43 @@
-import { Container, makeStyles } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
 
 const useStyles = makeStyles((theme) => ({}));
 
 const Leftbar = () => {
   const classes = useStyles();
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <HomeIcon className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+    </Container>
+  );
 };
 
 export default Leftbar;
