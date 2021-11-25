79ac8b95482646046036379f6f4edb21600b84e5
ADE <adejumoahmad4life@gmail.com>
2021-11-25 13:17:33 +0100

    added icons to the leftbar

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,6 +1,15 @@
 import { Container, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import HomeIcon from "@mui/icons-material/Home";
+import Person from "@mui/icons-material/Person";
+import ListIcon from "@mui/icons-material/List";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import PlayCircleOutline from "@mui/icons-material/PlayCircleOutline";
+import TabletMacIcon from "@mui/icons-material/TabletMac";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -23,7 +32,14 @@ const useStyles = makeStyles((theme) => ({
       cursor: "pointer",
     },
   },
+  icon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "18px",
+    },
+  },
   text: {
+    fontWeight: "500",
     [theme.breakpoints.down("sm")]: {
       display: "none",
     },
@@ -39,28 +55,40 @@ const Leftbar = () => {
         <Typography className={classes.text}>Homepage</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Person className={classes.icon} />
+        <Typography className={classes.text}>Friends</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <ListIcon className={classes.icon} />
+        <Typography className={classes.text}>List</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <PhotoCameraIcon className={classes.icon} />
+        <Typography className={classes.text}>Camera</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <PlayCircleOutline className={classes.icon} />
+        <Typography className={classes.text}>Videos</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <TabletMacIcon className={classes.icon} />
+        <Typography className={classes.text}>Apps</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <BookmarkIcon className={classes.icon} />
+        <Typography className={classes.text}>Collections</Typography>
+      </div>
+      <div className={classes.item}>
+        <StorefrontIcon className={classes.icon} />
+        <Typography className={classes.text}>Market Place</Typography>
+      </div>
+      <div className={classes.item}>
+        <SettingsIcon className={classes.icon} />
+        <Typography className={classes.text}>Settings</Typography>
+      </div>
+      <div className={classes.item}>
+        <LogoutIcon className={classes.icon} />
+        <Typography className={classes.text}>Logout</Typography>
       </div>
     </Container>
   );
