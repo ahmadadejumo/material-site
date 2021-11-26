4ea795296c922f3bac99e4a3c99da28eaed605bd
ADE <adejumoahmad4life@gmail.com>
2021-11-26 21:59:31 +0100

    added an add icon button

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { Fab, makeStyles, Tooltip } from "@material-ui/core";
+import AddIcon from "@mui/icons-material/Add";
 
 const useStyles = makeStyles((theme) => ({}));
 
 const Feeds = () => {
   const classes = useStyles();
-  return <></>;
+  return (
+    <>
+      <Tooltip>
+        <Fab color="primary" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  );
 };
 
 export default Feeds;
