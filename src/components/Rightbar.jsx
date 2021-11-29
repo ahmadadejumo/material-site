b4451de530ecbef8d2f1a6eb5146e49f4092dd78
ADE <adejumoahmad4life@gmail.com>
2021-11-29 04:51:19 +0100

    added image section in the rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, makeStyles, Typography } from "@material-ui/core";
 import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -15,6 +17,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
+
 const Rightbar = () => {
   const classes = useStyles();
   return (
@@ -22,7 +75,7 @@ const Rightbar = () => {
       <Typography className={classes.title} gutterBottom>
         Online friends
       </Typography>
-      <AvatarGroup max={6}>
+      <AvatarGroup max={6} style={{ marginBottom: 20 }}>
         <Avatar
           alt="Remy Sharp"
           src="https://material-ui.com/static/images/avatar/1.jpg"
@@ -52,6 +105,21 @@ const Rightbar = () => {
           src="https://material-ui.com/static/images/avatar/7.jpg"
         />
       </AvatarGroup>
+      <Typography className={classes.title} gutterBottom>
+        Gallery
+      </Typography>
+      <ImageList cols={3} rowHeight={100} style={{ marginBottom: 20 }}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </Container>
   );
 };
